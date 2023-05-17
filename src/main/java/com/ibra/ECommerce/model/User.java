88b82265e7a6a228f93e39ed5e141aaebe1e9f51
Ibrahim <32011652+ibrahimGokdemir@users.noreply.github.com>
2023-05-17 23:45:07 +0300

    Created base classes for user

--- src/main/java/com/ibra/ECommerce/model/User.java
+++ src/main/java/com/ibra/ECommerce/model/User.java
@@ -1,5 +1,56 @@
 package com.ibra.ECommerce.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+
+
+@Entity
+@Table(name="users")
 public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(nullable = false)
+	private String firstName;
+	
+	@Column(nullable = false)
+	private String lastName;
+	
+	@Column(nullable = false, unique = true)
+	private String email;
+	
+	private String phoneNumber;
+	
+	private String city;
+	
+	@Column(nullable = false)
+	private Boolean active;
+	
+	@Override
+	public String toString() {
+		
+		return "User{" +
+				"id= "+id +
+				",firstName = " +firstName+
+				",lastName = " +lastName+
+				",eMail = "+email+
+				",phoneNumber = "+phoneNumber+
+				",city = "+city+
+				"}";
+		
+	}
+	
+	
 
 }
--- src/main/java/com/ibra/ECommerce/repository/UserRepository.java
+++ src/main/java/com/ibra/ECommerce/repository/UserRepository.java
@@ -1,5 +1,25 @@
 package com.ibra.ECommerce.repository;
 
-public class UserRepository {
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.ibra.ECommerce.model.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+	
+	User findByUser(String email);
+	
+	List<User> findByFirstName(String fistName);
+	
+	List<User>findByLastname(String lastName);
+	
+	List<User>findByFirstNameAndLastName (String firstName, String lastName);
+	
+	List<User>findByActiveTrue(boolean active);
+	
+	List<User>findByActiveFalse(boolean active);
 
 }
